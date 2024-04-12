bcf44d2b62d95a1a80cd7cb19647ff121b01da4c
= <=>
2024-04-12 10:25:03 -0300

    Create: Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../styles/header.css'
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    const [menu, setMenu] = useState<boolean>(false);
+    const [icon, setIcon] = useState(faBars);
+
+    const menuToggle = () => {
+        if(menu) {
+            setMenu(false);
+            setIcon(faBars)
+        } else {
+            setMenu(true)
+            setIcon(faClose)
+        }
+    }
+    
+    return (
+        <header className={`nav ${menu ? 'nav-active' : ''}`}>
+            <h1 className='title'>Blog K-12</h1>
+            <ul className='nav-links'>
+                <li className='nav-item'><a href="/posts">Posts</a></li>
+                <a className='login-button nav-item' href="/login"><li>Login</li></a>
+                <a className='register-button nav-item' href="/register"><li>Registre-se</li></a>
+            </ul>
+            <FontAwesomeIcon icon={icon} onClick={menuToggle} className='menu'/>
+        </header>
+    )
+}
+
+export default Header
