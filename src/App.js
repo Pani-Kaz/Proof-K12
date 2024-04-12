4b98e4522bb4ee8153ca401e1010939d0df84a22
= <=>
2024-04-12 15:35:26 -0300

    Create: Loading Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import Footer from './components/Footer.tsx';
 import Header from './components/Header.tsx'
 import AppRoutes from './router.js';
 import './styles/globals.css';
+import Loading from './pages/Loading.tsx';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setLoading(false);
+    });
+  }, []);
+  
   return (
     <>
       <Header />
       <AppRoutes />
       <Footer />
+      {loading && <Loading />}
     </>
   );
 }
